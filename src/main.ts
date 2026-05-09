de9baef4398cc442a241834666a54fab5bc57145
Walter Cervini <wcervini@gmail.com>
2026-05-10 00:12:46 +0100

    feat(UI): Add modal functionality and Apache License

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import "./styles.css";
+
+const boton = document.getElementById("boton");
+const modal = document.getElementById("modal");
+const cerrar = document.getElementById("close");
+boton.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+});
+
+cerrar.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
